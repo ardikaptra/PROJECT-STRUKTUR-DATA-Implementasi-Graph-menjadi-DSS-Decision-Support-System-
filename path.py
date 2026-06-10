4a3acbad3693e73d59b69edf3bf399f27aed194f
ardikaptra <ardika1putra17@gmail.com>
2026-06-10 20:54:26 +0800

    Update path.py

--- path.py
+++ path.py
@@ -1,29 +1,32 @@
 import heapq
 
-def shortest_path(graph, start, goal):
+def shortest_path(graph,start,goal):
 
-    queue = [(0, start, [])]
+    queue = [(0,start,[start])]
 
     visited = set()
 
     while queue:
 
-        cost, node, path = heapq.heappop(queue)
+        cost,node,path = heapq.heappop(queue)
+
+        if node == goal:
+            return cost,path
 
         if node in visited:
             continue
 
-        path = path + [node]
-
-        if node == goal:
-            return cost, path
-
         visited.add(node)
 
-        for neighbor, weight in graph[node].items():
+        for neighbor,weight in graph[node].items():
+
             heapq.heappush(
                 queue,
-                (cost + weight, neighbor, path)
+                (
+                    cost + weight,
+                    neighbor,
+                    path + [neighbor]
+                )
             )
 
-    return float("inf"), []
+    return None,None
